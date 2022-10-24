9c1467a05e175aaa820b914c2059c20af37a70fd
zhouhouyue <1282054757@qq.com>
2022-10-24 23:51:15 +0800

    修改告警
    Signed-off-by: zhouhouyue <1282054757@qq.com>

--- lite/build.sh
+++ lite/build.sh
@@ -32,43 +32,12 @@ WIFIIOT_ACTS_MODULES="${WIFIIOT_ACTS_MODULES},//test/xts/acts/distributed_schedu
 WIFIIOT_ACTS_MODULES="${WIFIIOT_ACTS_MODULES},//test/xts/acts/update_lite/dupdate_hal:ActsUpdaterFuncTest"
 WIFIIOT_ACTS_MODULES="${WIFIIOT_ACTS_MODULES},//test/xts/acts/startup_lite/bootstrap_hal:ActsBootstrapTest"
 
+source $BASE_HOME/test/xts/tools/lite/buildFun.sh
+
 error_report() {
     echo "Error on line $1"
 }
 trap 'error_report $LINENO' ERR
-
-
-usage()
-{
-  echo
-  echo "USAGE"
-  echo "       ./build.sh product=PRODUCT [platform=PLATFORM] [target=TARGET] xts=XTS"
-  echo
-  echo "                  product   : PRODUCT  product name, such as ipcamera or wifiiot"
-  echo "                  platform  : PLATFORM  the platform of device"
-  echo "                  target    : TARGET   the target for build, such as //xts/acts/communication_lite/wifiaware_test."
-  echo "                  xts       : XTS   the name of xts, such as acts/hits etc."
-  echo
-  exit 1
-}
-
-check_python()
-{
-  python_cmd=""
-  ver=$(python -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major);')
-  if [ "$ver" = "3" ];then
-    python_cmd=python
-  else
-    ver=$(python3 -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(major);')
-    if [ "$ver" = "3" ];then
-      python_cmd=python3
-    fi
-  fi
-  if [ -z "$python_cmd" ];then
-    echo "Enviroment variable 'python3' is required, and python verion must be greater than 3.7"
-    exit 1
-  fi
-}
 
 parse_cmdline()
 {
@@ -115,8 +84,6 @@ parse_cmdline()
   fi
 }
 
-
-
 build()
 {
   out_dir="${BASE_HOME}/out/hispark_pegasus/wifiiot_hispark_pegasus"
@@ -140,57 +107,8 @@ build()
     fi
   else
     #python build.py ${PRODUCT}_${PLATFORM} -b debug --test xts $TARGET
-    suite_root_dir="${BASE_HOME}/out/${PRODUCT}/${PRODUCT}/suites"
-    xts_root_dir="${suite_root_dir}/acts"
-    suite_out_zip="${xts_root_dir}.zip"
-    python build.py -p ${PRODUCT}@${PLATFORM} -f --gn-args build_xts=true
-    mkdir -p ${xts_root_dir}/testcases/${PRODUCT}
-    cp -f ${BASE_HOME}/out/${PRODUCT}/${PRODUCT}/OHOS_Image_allinone.bin ${suite_root_dir}/acts/testcases/${PRODUCT}/OHOS_Image_allinone.bin
-    python test/xts/tools/lite/build/utils.py --method_name generate_allinone_testjson_by_template --arguments tmpl_file=${BASE_HOME}/test/xts/acts/build_lite/Test.tmpl#module_name=OHOS_Image_allinone#product_name=${PRODUCT}#config_file=${xts_root_dir}/testcases/${PRODUCT}/OHOS_Image_allinone.json
-      
-    cd $suite_root_dir
-    rm -f ${suite_out_zip}
-    zip -rv ${suite_out_zip} acts
-    cd $BASE_HOME
+    build_niobe407
   fi
-}
-
-build_wifiiot()
-{
-    current_xts=$1
-    current_target=$2
-    xts_root_dir="${suite_root_dir}/${current_xts}"
-    suite_out_dir="${xts_root_dir}/testcases"
-    suite_out_zip="${xts_root_dir}.zip"
-    mkdir -p $DIST_DIR
-    IFS=',' read -r -a array <<< "${current_target}"
-    echo "--------------------------------------------${array[@]}"
-    set -e
-	mkdir -p ${DIST_DIR}/json
-    for element in ${array[*]}
-    do
-      python build.py -p wifiiot_hispark_pegasus@hisilicon -f --test xts ${element} --gn-args build_xts=true
-      suite_build_target=$(echo "${element}" | awk -F "[/:]" '{print $NF}')
-      module_list_file=$suite_out_dir/module_info.json
-      suite_module_name=$(python test/xts/tools/lite/build/utils.py --method_name get_modulename_by_buildtarget --arguments module_list_file=${module_list_file}#build_target=${suite_build_target})
-      subsystem_name=$(python test/xts/tools/lite/build/utils.py --method_name get_subsystem_name --arguments path=${element})
-	  
-      python test/xts/tools/lite/build/utils.py --method_name record_testmodule_info --arguments build_target_name=${suite_module_name}#module_name=${suite_module_name}#subsystem_name=${subsystem_name}#suite_out_dir=${DIST_DIR}/json#same_file=True
-
-      mkdir -p ${suite_out_dir}/${subsystem_name}
-      cp -f ${BASE_HOME}/out/hispark_pegasus/wifiiot_hispark_pegasus/${WIFIIOT_OUTFILE} ${suite_out_dir}/${subsystem_name}/${suite_module_name}.bin
-      rm -f ${suite_out_dir}/${subsystem_name}/*.a
-      cp -rf ${xts_root_dir}  ${DIST_DIR}
-    done
-	
-    cp -rf ${DIST_DIR}/${current_xts} ${suite_root_dir}
-    rm -f ${suite_out_dir}/.bin
-    cp -rf ${DIST_DIR}/json/module_info.json ${suite_out_dir}
-    cd $suite_root_dir
-    rm -f ${suite_out_zip}
-    zip -rv ${suite_out_zip} ${current_xts}
-    cd $BASE_HOME
-
 }
 
 echo $BASE_HOME
