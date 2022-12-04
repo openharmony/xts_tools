f220c97e88ed147592b44b2de24a3c31cf2d3280
zhouhouyue <1282054757@qq.com>
2022-12-04 13:42:51 +0000

    update lite/buildFun.sh.
    Signed-off-by: zhouhouyue <1282054757@qq.com>

--- lite/buildFun.sh
+++ lite/buildFun.sh
@@ -96,10 +96,11 @@ build_common()
     mkdir -p ${xts_root_dir_common}/testcases/${PRODUCT}
     if [[ "$PRODUCT" == "neptune_iotlink_demo" ]]; then
       cp -f $(dirname $suite_root_dir_common)/bin/hihope_neptune100.fls ${suite_root_dir_common}/acts/testcases/${PRODUCT}/OHOS_Image.fls
+      python test/xts/tools/lite/build/utils.py --method_name generate_allinone_testjson_by_template --arguments tmpl_file=${BASE_HOME}/test/xts/acts/build_lite/Test.tmpl#module_name=OHOS_Image.fls#product_name=${PRODUCT}#config_file=${xts_root_dir_common}/testcases/${PRODUCT}/OHOS_Image.json
     else
       cp -f $(dirname $suite_root_dir_common)/OHOS_Image.bin ${suite_root_dir_common}/acts/testcases/${PRODUCT}/OHOS_Image.bin
+      python test/xts/tools/lite/build/utils.py --method_name generate_allinone_testjson_by_template --arguments tmpl_file=${BASE_HOME}/test/xts/acts/build_lite/Test.tmpl#module_name=OHOS_Image.bin#product_name=${PRODUCT}#config_file=${xts_root_dir_common}/testcases/${PRODUCT}/OHOS_Image.json
     fi
-    python test/xts/tools/lite/build/utils.py --method_name generate_allinone_testjson_by_template --arguments tmpl_file=${BASE_HOME}/test/xts/acts/build_lite/Test.tmpl#module_name=OHOS_Image#product_name=${PRODUCT}#config_file=${xts_root_dir_common}/testcases/${PRODUCT}/OHOS_Image.json
     mv $xts_root_dir_common/test_component.json ${xts_root_dir_common}/testcases/test_component.json
     echo "{}" > ${xts_root_dir_common}/testcases/module_info.json
     python test/xts/tools/lite/build/utils.py --method_name record_testmodule_info --arguments build_target_name=OHOS_Image#module_name=OHOS_Image#subsystem_name=${PRODUCT}#suite_out_dir=${xts_root_dir_common}/testcases#same_file=True
