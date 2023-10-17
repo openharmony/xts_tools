a3528b3822e20b1c3a5d9b4f01597b1c5adc4fc0
zhaoxin24742 <zhaoxin24742@openvalley.net>
2023-10-17 11:09:38 +0800

    feat(application):添加打印关闭指令
    Signed-off-by: zhaoxin24742 <zhaoxin24742@openvalley.net>

--- sample/AppSampleD/hvigorw.bat
+++ sample/AppSampleD/hvigorw.bat
@@ -11,6 +11,7 @@
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
 
+@Echo off
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
