e7bc2b4b9e9be76cb43658511b5e9bd010b66d0a
guoxudong <guoxudong12@huawei.com>
2025-06-10 11:37:58 +0800

    将0328分支代码合入到0603
    Signed-off-by: guoxudong <guoxudong12@huawei.com>

---
+++ build/copy_hypium_static.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright (C) 2025 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+echo exec $@
+
+code_root=$(realpath $(dirname $(readlink -f $0))/../../../../)
+
+cp -rf ${code_root}/test/xts/tools/hypium_static/* $1/entry/src/
