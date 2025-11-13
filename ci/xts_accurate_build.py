6bc2f93a1bd6f9cad907cfc2898e22a295a84cb5
yuanjiazheng <yuanjiazheng1@h-partners.com>
2025-11-13 16:37:20 +0800

    Refine accurate build code.
    Signed-off-by: yuanjiazheng <yuanjiazheng1@h-partners.com>

--- ci/xts_accurate_build.py
+++ ci/xts_accurate_build.py
@@ -17,134 +17,336 @@
 
 import io
 import os
-import shutil, json
+import abc
+import json
+import shutil
 import subprocess
-from typing import List, Dict
+from typing import List
 from Utils import XTSLogger
 
-def calc_final_targets(out_path: str, gn_path: str, targets: List[str], env: Dict | None = None):
-    """
-    Retrive xts accurate build targes from GN dependency tree.
 
-    Args:
-        out_path: The output dir of GN gen (abspath required).
-        gn_path: The GN executable path (abspath required).
-        targets: Original targets for xts accurate build.
-        env: Environment dict.
+class XtsAccurateBuild:
+    def __init__(self, code_base: str, args: List[str], root_target: str):
+        self._code_base = code_base
+        self._args = args
+        self._logger = XTSLogger()
+        self._out_path = os.path.join(code_base, "out")
+        self._gn_path = os.path.join(code_base, "prebuilts/build-tools/linux-x86/bin/gn")
+        self._root_target = root_target
+        self._build_targets = set()
+        self._env = os.environ.copy()
 
-    Returns:
-        New xts accurate build target list; None on error.
-    """
-    logger = XTSLogger()
-    # keep virtual or non-acts targets.
-    keep_set = {
-        "test/xts/acts:xts_acts",
-        "test/xts/dcts:xts_dcts",
-        "test/xts/hats:xts_hats",
-        "test/xts/hits:xts_hits",
-        "test/xts/tools:xts_tools"
-    }
+    @property
+    def code_base(self):
+        return self._code_base
 
-    _targets, final_tgts = set(targets), set()
-    for tgt in _targets:
-        if tgt in keep_set or not tgt.startswith("test/xts/acts"):
-            final_tgts.add(tgt)
-            logger.info(f"Keep target: '{tgt}'")
-    _targets.difference_update(final_tgts)
+    @property
+    def args(self):
+        return self._args
 
-    if len(_targets) == 0:
-        final_tgts = list(final_tgts)
-        logger.info(f"Keep all targets, build targets: {final_tgts}")
-        return final_tgts
+    @args.setter
+    def args(self, value: List[str]):
+        self._args = value
 
-    try:
-        cmd_list = [
-            gn_path,
-            "desc",
-            out_path,
-            "//test/xts/acts:xts_acts",
-            "deps",
-            "--tree"
+    @property
+    def logger(self):
+        return self._logger
+
+    @property
+    def out_path(self):
+        return self._out_path
+
+    @out_path.setter
+    def out_path(self, value: str):
+        self._out_path = value
+
+    @property
+    def gn_path(self):
+        return self._gn_path
+
+    @property
+    def root_target(self):
+        return self._root_target
+
+    @root_target.setter
+    def root_target(self, value: str):
+        self._root_target = value
+
+    @property
+    def build_targets(self):
+        return self._build_targets
+
+    @property
+    def env(self):
+        return self._env
+
+    @abc.abstractmethod
+    def _process_args(self) -> None:
+        """
+        This would be called before self._calc_final_targets.
+            In here, you are advised to:
+            - justify self._args;
+            - setup self._env, self._build_targets, self._gn_path, etc.
+        """
+        pass
+
+    def _generate(self):
+        args = [self._args[0], "--build-only-gn"]
+        args.extend(self._args[1:])
+        self._logger.info(">>> Compile command: {}".format(str.join(' ', args)))
+        return subprocess.run(args).returncode
+
+    @abc.abstractmethod
+    def _calc_immuned_targets(self) -> set[str]:
+        """
+        Returns:
+            A set of targets to keep. No? just provide an empty set.
+        """
+        pass
+
+    @abc.abstractmethod
+    def _pick_target(self, target: str) -> bool:
+        """
+        Tell if the target should be picked.
+        """
+        pass
+
+    def _calc_final_targets(self):
+        """
+        Retrive xts accurate build targes from GN dependency tree.
+        """
+        _targets, final_tgts = self._build_targets.copy(), set()
+        final_tgts = self._calc_immuned_targets()
+        _targets.difference_update(final_tgts)
+
+        if len(_targets) == 0:
+            self._logger.info(f"Keep all targets, build targets: {list(final_tgts)}")
+            return
+
+        ninja_ctx = [
+            os.path.join(self._out_path, "build.ninja"),
+            os.path.join(self._out_path, "build.ninja.d"),
+            os.path.join(self._out_path, "build.ninja.stamp")
         ]
-        logger.info(">>> Execute command: {}".format(str.join(' ', cmd_list)))
 
-        proc = subprocess.Popen(
-            cmd_list,
-            stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE,
-            env = env
-        )
+        try:
+            # backup ninja context.
+            for ctx in ninja_ctx:
+                shutil.copy2(ctx, f"{ctx}.bkp")
 
-        logger.info("Start picking xts build targets from dep tree, "
-                    "this would take a short while...")
+            cmd_list = [
+                self._gn_path,
+                "desc",
+                self._out_path,
+                self._root_target,
+                "deps",
+                "--tree"
+            ]
+            self._logger.debug(json.dumps(self._env, indent = 2, sort_keys = False))
+            self._logger.info(">>> Execute command: {}".format(str.join(' ', cmd_list)))
 
-        with io.TextIOWrapper(proc.stdout, encoding = "utf-8") as stream:
-            for ln in stream:
-                ln = ln.strip()
-                if ln.startswith("//test/xts/acts"):
-                    ln = ln[2:]
-                    if ln in _targets:
-                        final_tgts.add(ln)
-                        _targets.remove(ln)
-                        logger.logging_phase = "TARGET PICK"
-                        logger.info(f"Pick target: '{ln}'")
-                else:
-                    logger.logging_phase = "GN DESC"
-                    logger.info(ln)
+            proc = subprocess.Popen(
+                cmd_list,
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE,
+                env = self._env
+            )
 
-        rc = proc.wait()
+            self._logger.info("Start picking xts build targets from dep tree, "
+                              "this would take a short while...")
+
+            with io.TextIOWrapper(proc.stdout, encoding = "utf-8") as stream:
+                for ln in stream:
+                    ln = ln.strip()
+                    if self._pick_target(ln):
+                        ln = ln[2:]
+                        if ln in _targets:
+                            final_tgts.add(ln)
+                            _targets.remove(ln)
+                            self._logger.logging_phase = "TARGET PICK"
+                            self._logger.info(f"Pick target: '{ln}'")
+                    else:
+                        self._logger.logging_phase = "GN DESC"
+                        self._logger.info(ln)
+
+            rc = proc.wait()
+            if rc != 0:
+                err_msg = proc.stderr.read().decode("utf-8")
+                self._logger.error(f"command '{str.join(' ', cmd_list)}' exited with code: {rc}")
+                self._logger.error(f"Error Message: {err_msg}")
+                raise Exception("GN command returned unexpectedly.")
+        except Exception as e:
+            self._logger.error(e)
+            self._logger.error("Something went wrong "
+                        "while calculating xts final build targets, "
+                        "fall back to the original build targets")
+            final_tgts = None
+        finally:
+            self._logger.logging_phase = None
+            # restore ninja context.
+            for ctx in ninja_ctx:
+                os.remove(ctx)
+                os.rename(f"{ctx}.bkp", ctx)
+                os.utime(ctx)
+
+            if final_tgts is not None:
+                self._build_targets = final_tgts
+                self._logger.info(f"Pick done, build targets: {list(self._build_targets)}")
+                self._logger.info(f"Discarded targets: {list(_targets)}")
+
+    @abc.abstractmethod
+    def _ninja(self) -> int:
+        pass
+
+    def build(self):
+        rc = self._generate()
         if rc != 0:
-            err_msg = proc.stderr.read().decode("utf-8")
-            logger.error(f"command '{str.join(' ', cmd_list)}' exited with code: {rc}")
-            logger.error(f"Error Message: {err_msg}")
-            raise Exception("GN command returned unexpectedly.")
-    except Exception as e:
-        logger.error(e)
-        logger.error("Something went wrong "
-                     "while calculating xts final build targets, "
-                     "fall back to the original build targets")
-        final_tgts = None
-    finally:
-        logger.logging_phase = None
-        if final_tgts is not None:
-            final_tgts = list(final_tgts)
-            logger.info(f"Pick done, build targets: {final_tgts}")
-            logger.info(f"Discarded targets: {list(_targets)}")
-        return final_tgts
+            return rc
+        self._process_args()
+        self._calc_final_targets()
+        rc = self._ninja()
+        return rc
 
 
-def parse_args(args: List[str]):
-    focus_set = {
-        "--abi-type", "--device-type", 
-        "--product-name", "--build-target", "--export-para"
-    }
-    ret, _args, shift = {"targets": [], "env": {}}, [], False
-    for idx, arg in enumerate(args):
-        if shift:
-            shift = False
-            continue
-        if arg in focus_set:
-            shift = True
-            if arg == "--build-target":
-                ret.get("targets").append(args[idx + 1])
+class ActsBuildUtil():
+    @staticmethod
+    def calc_immuned_targets(ab: XtsAccurateBuild) -> set[str]:
+        # reserve virtual or non-acts targets.
+        immuned_set = {
+            "test/xts/acts:xts_acts",
+            "test/xts/dcts:xts_dcts",
+            "test/xts/hats:xts_hats",
+            "test/xts/hits:xts_hits",
+            "test/xts/tools:xts_tools"
+        }
+        keep_tgts = set()
+        for tgt in ab._build_targets:
+            if tgt in immuned_set or not tgt.startswith("test/xts/acts"):
+                keep_tgts.add(tgt)
+                ab.logger.info(f"Keep target: '{tgt}'")
+        return keep_tgts
+
+    @staticmethod
+    def pick_target(target: str) -> bool:
+        return target.startswith("//test/xts/acts")
+
+    @staticmethod
+    def process_targets(ab: XtsAccurateBuild) -> List[str]:
+        _args, shift = [], False
+        for idx, arg in enumerate(ab.args):
+            if shift:
+                shift = False
                 continue
-            elif arg == "--export-para":
-                env_arg = args[idx + 1].split(':')
-                ret.get("env")[env_arg[0]] = env_arg[1]
+            if arg == "--build-target":
+                if ab.args[idx + 1] in ab.build_targets:
+                    _args.extend([arg, ab.args[idx + 1]])
+                shift = True
+                continue
             else:
-                ret[arg[2:]] = args[idx + 1]
-            _args.extend([arg, args[idx + 1]])
-            continue
-        elif arg == "--prebuilts-sdk-gn-args":
-            # Do make sure the '--prebuilts-sdk-gn-args'
-            # being the last named arg in arg list.
-            ret["prebuilts-sdk-gn-args"] = args[idx:]
-            return ret, _args
-        _args.append(arg)
-    return ret, _args
+                _args.append(arg)
+        return _args
+
+    @staticmethod
+    def exec_ninja(ab: XtsAccurateBuild):
+        args = ActsBuildUtil.process_targets(ab)
+        args = [args[0], "--fast-rebuild"] + args[1:]
+        ab.logger.info(">>> Compile command: {}".format(str.join(' ', args)))
+        return subprocess.run(args).returncode
 
 
-def xts_accurate_build(code_base: str, args: List[str]):
+class OpenSourceBuild(XtsAccurateBuild):
+    def __init__(self, code_base: str, args: List[str]):
+        super().__init__(code_base, args, "//test/xts/acts:xts_acts")
+
+    def _process_args(self):
+        prod_name = ""
+        rtrim_idx, shift = -1, False
+
+        for idx, arg in enumerate(self.args):
+            if shift:
+                shift = False
+                continue
+            if arg == "--build-target":
+                self.build_targets.add(self.args[idx + 1])
+                shift = True
+            elif arg == "--product-name":
+                prod_name = self.args[idx + 1]
+                shift = True
+            elif arg == "--prebuilts-sdk-gn-args":
+                rtrim_idx = idx
+                break
+
+        self.out_path = os.path.join(self.out_path, prod_name)
+        if rtrim_idx != -1:
+            self.args = self.args[0:rtrim_idx]
+
+    def _calc_immuned_targets(self) -> set[str]:
+        return ActsBuildUtil.calc_immuned_targets(self)
+
+    def _pick_target(self, target: str) -> bool:
+        return ActsBuildUtil.pick_target(target)
+
+    def _ninja(self):
+        return ActsBuildUtil.exec_ninja(self)
+
+
+class ProprietaryBuild(XtsAccurateBuild):
+    def __init__(self, code_base, args):
+        super().__init__(code_base, args, "//test/xts/acts:xts_acts")
+
+    def _process_args(self):
+        class Operator:
+            NOOP = 0
+            LOAD = 1
+            SHIFT = 2
+            SHIFT_LOAD = 3
+
+        abi_type, device_type = "", ""
+        args, op = [], Operator.LOAD
+
+        for idx, arg in enumerate(self.args):
+            if op == Operator.SHIFT or \
+                op == Operator.SHIFT_LOAD:
+                op = Operator.LOAD
+                continue
+
+            if arg == "--build-target":
+                self.build_targets.add(self.args[idx + 1])
+                op = Operator.SHIFT_LOAD
+            elif arg == "--abi-type":
+                abi_type = self.args[idx + 1]
+                op = Operator.SHIFT_LOAD
+            elif arg == "--device-type":
+                device_type = self.args[idx + 1]
+                op = Operator.SHIFT_LOAD
+            elif arg == "--export-para":
+                env_arg = self.args[idx + 1].split(':')
+                self.env[env_arg[0]] = env_arg[1]
+                op = Operator.SHIFT_LOAD
+            elif arg == "--build-ohos-sdk":
+                op = Operator.NOOP
+            elif arg == "--gn-args" and self.args[idx + 1] == "sdk_build_arkts=true":
+                op = Operator.SHIFT
+
+            if op == Operator.LOAD:
+                args.append(arg)
+            elif op == Operator.SHIFT_LOAD:
+                args.extend([arg, self.args[idx + 1]])
+
+        self.out_path = os.path.join(self.out_path, abi_type, device_type)
+        self.args = args
+
+    def _calc_immuned_targets(self) -> set[str]:
+        return ActsBuildUtil.calc_immuned_targets(self)
+
+    def _pick_target(self, target: str) -> bool:
+        return ActsBuildUtil.pick_target(target)
+
+    def _ninja(self):
+        return ActsBuildUtil.exec_ninja(self)
+
+
+def xts_accurate_build(code_base: str, args: List[str], proprietary_build = False):
     """
     Execute xts accurate build.
 
@@ -152,72 +354,14 @@ def xts_accurate_build(code_base: str, args: List[str]):
         code_base: abspath of ohos source code.
         out_path: GN build output abspath.
         args: _CMD to run, as in subprocess.run(_CMD).
-    
+        proprietary_build: build in proprietary mode.
+
     Returns:
         0 on success, otherwise on failure.
     """
-    os.chdir(code_base)
-    logger = XTSLogger()
-
-    def ext_build_target(args: List[str], targets: List[str]):
-        for tgt in targets:
-            args.extend(["--build-target", tgt])
-        return args
-
-    arg_dict, base_args = parse_args(args)
-    targets = arg_dict.get("targets", [])
-    prebuilts_sdk_gn_args = arg_dict.get("prebuilts-sdk-gn-args", [])
-    _args = ext_build_target(base_args.copy(), targets)
-    _args.append("--build-only-gn")
-    _args.extend(prebuilts_sdk_gn_args)
-
-    # gn gen
-    logger.info(">>> Compile command: {}".format(str.join(' ', _args)))
-    rc = subprocess.run(_args).returncode
-    if (rc != 0):
-        return rc
-
-    # gn desc
-    gn_path = os.path.join(code_base, "prebuilts/build-tools/linux-x86/bin/gn")
-    out_path = os.path.join(code_base, "out")
-
-    if arg_dict.get("abi-type") is not None:
-        out_path = os.path.join(out_path, arg_dict.get("abi-type"), arg_dict.get("device-type"))
+    if proprietary_build:
+        pb = ProprietaryBuild(code_base, args)
+        return pb.build()
     else:
-        out_path = os.path.join(out_path, arg_dict.get("product-name"))
-
-    # backup ninja context.
-    ninja_ctx = [
-        os.path.join(out_path, "build.ninja"),
-        os.path.join(out_path, "build.ninja.d"),
-        os.path.join(out_path, "build.ninja.stamp")
-    ]
-
-    for ctx in ninja_ctx:
-        shutil.copy2(ctx, f"{ctx}.bkp")
-
-    env = os.environ.copy()
-    env.update(arg_dict.get("env", {}))
-    logger.debug(json.dumps(env, indent = 2, sort_keys = False))
-    _tgts = calc_final_targets(out_path, gn_path, targets, env)
-
-    if _tgts is None:
-        _args = ext_build_target(base_args.copy(), targets)
-    elif len(_tgts) > 1 or not \
-        (len(_tgts) == 1 and _tgts[0] == "deploy_testtools"):
-        _args = ext_build_target(base_args.copy(), _tgts)
-    else:
-        logger.info("XTS accurate target list is empty, skip building.")
-        return 0
-
-    # restore ninja context.
-    for ctx in ninja_ctx:
-        os.remove(ctx)
-        os.rename(f"{ctx}.bkp", ctx)
-        os.utime(ctx)
-
-    # ninja
-    _args.append("--fast-rebuild")
-    _args.extend(prebuilts_sdk_gn_args)
-    logger.info(">>> Compile command: {}".format(str.join(' ', _args)))
-    return subprocess.run(_args).returncode
+        ob = OpenSourceBuild(code_base, args)
+        return ob.build()
